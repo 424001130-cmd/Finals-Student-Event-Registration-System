15233dd3095e8965636160240df1de635b614673
424002321 <424002321@ntc.edu.ph>
2026-05-23 00:14:33 +0800

    Update register-event.php

--- student/register-event.php
+++ student/register-event.php
@@ -1,1 +1,23 @@
+<?php
+session_start();
+include __DIR__ . '/../config/db.php';
 
+if(!isset($_SESSION['user_id'])){
+    header("Location: ../auth/login.php");
+    exit();
+}
+
+$user_id = $_SESSION['user_id'];
+$event_id = $_POST['event_id'];
+
+$sql = "INSERT INTO registrations (user_id, event_id)
+        VALUES ('$user_id', '$event_id')";
+
+// magat
+if(mysqli_query($conn, $sql)){
+    header("Location: my-events.php");
+    exit();
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+?>
